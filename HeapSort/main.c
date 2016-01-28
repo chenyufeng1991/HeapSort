2c99e5fc6d33070ad009402e0ce7765f1334bd14
chenyufeng <503434355@qq.com>
2016-01-28 21:51:50 +0800

    4

--- HeapSort/main.c
+++ HeapSort/main.c
@@ -24,7 +24,7 @@ void HeapAdjust(int *a,int i,int size){  //调整堆
 
     int lchild = 2 * i;       //i的左孩子节点序号
     int rchild = 2 * i + 1;     //i的右孩子节点序号
-    int max = i;            //临时变量
+    int max = i;            //临时变量，存储下标值
     if(i <= size / 2){          //如果i不是叶节点就不用进行调整
         if(lchild <= size && a[lchild] > a[max]){
             max = lchild;
@@ -39,31 +39,28 @@ void HeapAdjust(int *a,int i,int size){  //调整堆
     }
 }
 
-void swap(int *first,int *second){
-    int temp;
-
-    temp = *first;
-    *first = *second;
-    *second = temp;
-}
-
-
 void BuildHeap(int *a,int size){    //建立堆
-
     for(int i = size / 2;i >= 1;i--){    //非叶节点最大序号值为size/2
         HeapAdjust(a,i,size);
     }
 }
 
 void HeapSort(int *a,int size){    //堆排序
-
+    //首先要建立一个堆
     BuildHeap(a,size);
     for(int i = size;i >= 1;i--){
 
         swap(&a[1],&a[i]);           //交换堆顶和最后一个元素，即每次将剩余元素中的最大者放到最后面
-        //BuildHeap(a,i-1);        //将余下元素重新建立为大顶堆
         HeapAdjust(a,1,i - 1);      //重新调整堆顶节点成为大顶堆
     }
+}
+
+void swap(int *first,int *second){
+    int temp;
+
+    temp = *first;
+    *first = *second;
+    *second = temp;
 }
 
 int main(int argc, const char * argv[]) {
